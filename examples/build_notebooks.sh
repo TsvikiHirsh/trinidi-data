1e6605465a1f07192af0f833c331300c8f8fa891
Thilo Balke <thilo.balke@gmail.com>
2023-04-10 14:44:19 -0600

    add new script builder

--- examples/build_notebooks.sh
+++ examples/build_notebooks.sh
@@ -3,11 +3,26 @@
 # Builds example notebooks and converts the files to python scripts.
 cd "${0%/*}" # go to 
 
-jupyter run notebooks/*.ipynb
-jupyter nbconvert --to python notebooks/*.ipynb  --output-dir scripts/
+###################################### from notebook to script
+# jupyter run notebooks/*.ipynb
+# jupyter nbconvert --to python notebooks/*.ipynb  --output-dir scripts/
 
 
+# for f in ./scripts/*.py; do
+#     sed -E -i '' '/^# In\[[0-9]+\]:/,+2d' $f
+# done
+########################################
+
+
+# from script to notebook
 for f in ./scripts/*.py; do
-    sed -E -i '' '/^# In\[[0-9]+\]:/,+2d' $f
+    filename=$(basename -- "$f")
+    outfile="./notebooks/${filename%.*}.ipynb"
+
+    python -m py2jn "$f" "$outfile"
+
 done
+
+jupyter nbconvert --to=notebook --inplace --execute notebooks/*.ipynb
+jupyter nbconvert --to=html notebooks/*.ipynb
 
